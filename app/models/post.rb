8733cf0ffedd3cab02c6c0e3daaf5db8f0920494
habin <habin201291@gmai.com>
2014-01-13 11:52:15 +0700

    edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
 	include Bootsy::Container
+	validates :title, :presence => {:message => "can't be blank"}
+	validates :content,  
+				:length => {:minimum => 5, :message => "has invalid length"}, 
+				:presence => {:message => "can't be blank"}
 end
